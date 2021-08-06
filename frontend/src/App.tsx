72119d9cd00f86e930b2c25b3a4a469176420e72
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2021-08-06 13:48:30 -0300

    install Intial Dependences

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
+    <BrowserRouter>
+        <Switch>
+          <h1>Hello World !!</h1>
+        </Switch>
+    </BrowserRouter>
   );
 }
 
