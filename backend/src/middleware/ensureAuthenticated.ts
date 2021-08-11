c539a139962168c5af3406d426f0756279686730
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2021-08-11 13:08:54 -0300

    Install Dependences and config Authenticated

---
+++ backend/src/middleware/ensureAuthenticated.ts
@@ -0,0 +1,45 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+import authConfig from '../config/auth';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    return response.status(401).json({ error: 'No token provided.' });
+  }
+
+  const parts = authHeader.split(' ');
+
+  if (!(parts.length === 2)) {
+    return response.status(401).json({ error: 'Token error.' });
+  }
+
+  const [scheme, token] = parts;
+
+  if (!/^Bearer$/i.test(scheme)) {
+    return response.status(401).json({ error: 'Token bad formatted.' });
+  }
+
+  try {
+    const decoded = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decoded as TokenPayload;
+
+    request.entity = {
+      id: sub,
+    }
+
+    return next();
+    
+  } catch {
+    return response.status(400).json({ error: 'oken ivalid.' });
+  }
+}
