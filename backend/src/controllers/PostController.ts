8ff9cb7ded9ade62020c6f8dd1c383e93a774a72
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2021-08-14 20:59:09 -0300

    Post Controllers

--- backend/src/controllers/PostController.ts
+++ backend/src/controllers/PostController.ts
@@ -1,68 +1,69 @@
+import { Request, Response } from 'express';
+
 import Post from '../models/Post';
 import { PostType } from '../Types/PostType';
 
 const PostController = {
   
-  async create(post: PostType) {
+  async create(req: Request, res: Response) {
     try {
-      
+
+      const post = await Post.create(req.body);
+
+      return res.status(200).json(post);
 
     }catch (error) {
-      alert("error Post Exists");
+      return res.status(400).json("error Post Exists");
    }
   },
 
-  async read() {
+  async read(req: Request, res: Response) {
     try {
-      
-      return [
-        {
-          id: "teste",
-          title: "teste",
-          subtitle: "teste",
-          date: "teste",
-          image: "teste",
-          link: "teste"
-        }
-      ]
+
+      const posts = await Post.find({});
+
+      return res.status(200).json(posts);
       
     } catch (error) {
-      alert("Could not list.\n" + error);
+      return res.status(400).json("Could not list.\n" + error);
     }
   },
   
-  async update(post: PostType) {
+  async update(req: Request, res: Response) {
     try {
-      
+
+      const post = await Post.findById(req.body.id);
+      const response = await Post.updateOne({id: post.id}, req.body);
+
+      return res.status(200).json(response);
       
     } catch (error) {
-      alert("Could not update.\n" + error);
+      return res.status(400).json("Could not update.\n" + error);
     }
   },
   
-  async delete(elements: {id: string}[]) {
+  async delete(req: Request, res: Response) {
     try {
       
+      const post = await Post.findById(req.body.id);
+      const response = await Post.deleteOne({id: post.id});
+
+      return res.status(200).json(response);
 
     } catch (error) {
       alert("don't was not possible to delete.\n" + error);
     }
   },
 
-  async show(code: string) {
+  async show(req: Request, res: Response) {
     try {
-      
-      return {
-        id: "teste",
-        title: "teste",
-        subtitle: "teste",
-        date: "teste",
-        image: "teste",
-        link: "teste"
-      }
+
+      const post = await Post.findById(req.body.id);
+
+      return res.status(200).json(post);
 
     } catch (error) {
-      alert("Could not show.\n" + error);
+      return res.status(400).json("Could not show.\n" + error);
     }
   }
 }
