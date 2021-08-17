70a25ddcd9a8cea03d88b38a7200d63edc796d52
GabrielFSSantos <gabrielfelipesouzasantos@gmail.com>
2021-08-16 23:49:47 -0300

    Update Table Width

--- frontend/src/hooks/useTable.ts
+++ frontend/src/hooks/useTable.ts
@@ -12,12 +12,13 @@ type TableProps = {
 export type Row = { 
   _id: string;
   name?: string;
+
   cpf?: string;
   occupation?: string;
   sector?: string;
 
+  description?: string;
   quantDosage?: number;
-  dosage?: number;
 
   date?: string;
   title?: string;
@@ -32,10 +33,10 @@ export function useTable({type, search}: TableProps) {
   useEffect(() => {
     if(type === 'employees') {
       setColumns([
-        { selector: row => 'name', name: 'Nome', sortable: true, format: row => {return row.name.toString()}},
-        { selector: row => 'cpf', name: 'CPF', sortable: true, format: row => {return row.cpf.toString()}},
-        { selector: row => 'occupation', name: 'Função', sortable: true, format: row => {return row.occupation.toString()}},
-        { selector: row => 'sector', name: 'Setor', sortable: true, format: row => {return row.sector.toString()}}
+        { selector: row => 'name', name: 'Nome', sortable: true, format: row => {return row.name.toString()}, maxWidth: "22rem"},
+        { selector: row => 'cpf', name: 'CPF', sortable: true, format: row => {return row.cpf.toString()}, maxWidth: "22rem"},
+        { selector: row => 'occupation', name: 'Função', sortable: true, format: row => {return row.occupation.toString()}, maxWidth: "22rem"},
+        { selector: row => 'sector', name: 'Setor', sortable: true, format: row => {return row.sector.toString()}, maxWidth: "22rem"}
       ]);
       EmployeeController.read().then((employees) => { if(employees){
         if(search.trim() === '') {
@@ -53,9 +54,9 @@ export function useTable({type, search}: TableProps) {
     } 
     else if(type === 'vaccines') {
       setColumns([
-        { selector: row => 'name', name: 'Nome', sortable: true, format: row => {return row.name.toString()}},
-        { selector: row => 'quantDosage', name: 'Quantidade de dosagem', sortable: true, format: row => {return row.quantDosage.toString()}},
-        //{ selector: row => 'dosage', name: 'Dosagem', sortable: true, format: row => {return row.dosage.toString()}}
+        { selector: row => 'name', name: 'Nome', sortable: true, format: row => {return row.name.toString()}, maxWidth: "25rem"},
+        { selector: row => 'quantDosage', name: 'Quantidade de dosagem', sortable: true, format: row => {return row.quantDosage.toString()}, maxWidth: "25rem"},
+        { selector: row => 'description', name: 'Descrição', sortable: true, format: row => {return row.description.toString()}, maxWidth: "25rem"}
       ]);
       
       VaccineController.read().then((vaccines) => { if(vaccines){
@@ -74,10 +75,10 @@ export function useTable({type, search}: TableProps) {
     }
     else if(type === 'posts') {
       setColumns([
-        { selector: row => 'date', name: 'Data', sortable: true, format: row => {return row.date.toString()}},
-        { selector: row => 'title', name: 'Título', sortable: true, format: row => {return row.title.toString()}},
-        { selector: row => 'subtitle', name: 'Subtítulo', sortable: true, format: row => {return row.subtitle.toString()}},
-        { selector: row => 'link', name: 'Link', sortable: true, format: row => {return row.link.toString()}}
+        { selector: row => 'date', name: 'Data', sortable: true, format: row => {return row.date.toString()}, maxWidth: "10rem"},
+        { selector: row => 'title', name: 'Título', sortable: true, format: row => {return row.title.toString()}, maxWidth: "20rem"},
+        { selector: row => 'subtitle', name: 'Subtítulo', sortable: true, format: row => {return row.subtitle.toString()}, maxWidth: "24rem"},
+        { selector: row => 'link', name: 'Link', sortable: true, format: row => {return row.link.toString()}, maxWidth: "24rem"}
       ]);
       
       PostController.read().then((posts) => { if(posts){
